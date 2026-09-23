b5e6858e34ead9206ca1b949bb137b4bc932feb2
Anushka Vishwakarma <anushkavishwakarma227@gmail.com>
2026-09-23 22:47:41 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+ return {
+    base: '/PORTFOLIO-2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
